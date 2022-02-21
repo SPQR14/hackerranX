fe53fed021bf3dc3a29b663bdd447b4a82309a0c
SPQR14 <albertopicolara@gmail.com>
2022-02-21 17:56:22 -0600

    Hasta búsqueda binaria

--- Curso_Codigo_Maquina/busqueda_binaria.py
+++ Curso_Codigo_Maquina/busqueda_binaria.py
@@ -11,3 +11,38 @@
                                             Recursividad/Búsqueda binaria.
 ########################################################################################################################
 """
+
+lista = [11, 12, 13, 14, 15, 16, 17]
+
+
+def busqueda_iterartiva(lista, dato):
+    bajo = 0
+    alto = len(lista) - 1
+    centro = (bajo + alto) // 2
+    while bajo <= alto:
+        if lista[centro] == dato:
+            return centro
+        elif lista[centro] < dato:
+            bajo = centro + 1
+        else:
+            alto = centro - 1
+        centro = (bajo + alto) // 2
+    return -1  # No existe en la lista
+
+
+def busqueda_recursiva(lista, bajo, alto, dato):
+    if bajo > alto:  # caso base
+        return -1
+    centro = (bajo + alto) // 2  # caso recursivo
+    if lista[centro] == dato:
+        return centro
+    elif lista[centro] < dato:
+        return busqueda_recursiva(lista, centro + 1, alto, dato)
+    else:
+       return busqueda_recursiva(lista, bajo, centro - 1, dato)
+
+
+for i in range(10, 19):
+    print(i, busqueda_iterartiva(lista, i))
+    print(i, busqueda_recursiva(lista, 0, (len(lista) + 1), i))
+
